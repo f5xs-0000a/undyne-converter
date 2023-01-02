b42671ba9363cbdaaba80e5c624a30e7919c1a03
F5XS <f5xs.0000a@gmail.com>
2023-01-02 15:18:54 +0800

    Finished add_app; proceeding to testing

--- src/add_app.rs
+++ src/add_app.rs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25ed601c640ed286eb01029524e87d7a990f79fa2fd96a3e760bd25820f33e82
-size 3617
+oid sha256:d29ec7d0169ee9a9f6640be71720b2127569cfbf689bd767c20bfbc51ed8d266
+size 3642
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bb62f0cbfa780e2e54ed6378fd1922bc2277fdd49d3803f82d1ada44ee38407
-size 357
+oid sha256:cd2fa4304aa9039ca13d5220b91d308737e8e299449628bfc6f5c1ba1b3faba4
+size 382
