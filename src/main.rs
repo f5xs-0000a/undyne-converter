52f53b9c631dc7f25f10a85172483ff885958316
F5XS <f5xs.0000a@gmail.com>
2023-04-25 01:03:58 +0800

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,37 @@
-use futures::StreamExt as _;
-use tokio_util::codec::FramedRead;
-use tokio_util::codec::BytesCodec;
-use tokio::io::AsyncWriteExt;
+use std::{
+    net::SocketAddr,
+};
+
 use axum::{
-    body::{boxed, StreamBody},
-    extract::{Multipart, Query},
+    body::{
+        boxed,
+    },
+    extract::{
+        Multipart,
+    },
     http::StatusCode,
     response::Response,
-    routing::{on, MethodFilter},
+    routing::{
+        on,
+        MethodFilter,
+    },
     Router,
 };
-use futures::Stream;
-use futures::TryStreamExt;
-use std::{io, net::SocketAddr, pin::Pin};
-use tokio::io::{AsyncRead, AsyncWrite, BufWriter};
-use tokio_util::io::StreamReader;
-use tokio::fs::{File, OpenOptions};
-use std::io::Result as IoResult;
+use futures::{
+    TryStreamExt,
+};
+use tokio::{
+    fs::{
+        OpenOptions,
+    },
+    io::{
+        AsyncWriteExt,
+    },
+};
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new()
-        .route("/upload", on(MethodFilter::POST, upload));
+    let router = Router::new().route("/upload", on(MethodFilter::POST, upload));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
 
@@ -33,7 +43,7 @@ async fn main() {
 
 pub async fn upload(mut multipart: Multipart) -> Result<Response, StatusCode> {
     while let Some(mut field) = match multipart.next_field().await {
-        Err(e) => return Err(StatusCode::INTERNAL_SERVER_ERROR),
+        Err(_e) => return Err(StatusCode::INTERNAL_SERVER_ERROR),
         Ok(None) => None,
         Ok(Some(field)) => Some(field),
     } {
@@ -41,8 +51,13 @@ pub async fn upload(mut multipart: Multipart) -> Result<Response, StatusCode> {
             None => return Err(StatusCode::INTERNAL_SERVER_ERROR),
             Some(fname) => fname,
         };
-        let mut file = match OpenOptions::new().create(true).write(true).open(filename).await {
-            Err(e) => return Err(StatusCode::INTERNAL_SERVER_ERROR),
+        let mut file = match OpenOptions::new()
+            .create(true)
+            .write(true)
+            .open(filename)
+            .await
+        {
+            Err(_e) => return Err(StatusCode::INTERNAL_SERVER_ERROR),
             Ok(f) => f,
         };
 
@@ -52,5 +67,8 @@ pub async fn upload(mut multipart: Multipart) -> Result<Response, StatusCode> {
         file.flush().await.unwrap();
     }
 
-    Ok(Response::builder().status(StatusCode::CREATED).body(boxed("Ok".to_owned())).unwrap())
+    Ok(Response::builder()
+        .status(StatusCode::CREATED)
+        .body(boxed("Ok".to_owned()))
+        .unwrap())
 }
